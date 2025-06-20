f537a336e4891ed19a83c9c132b7bb8190a12a04
Dmitry Shadrunov <ionian.wind@gmail.com>
2025-06-21 00:35:10 +0300

    ui refactoring

---
+++ src/utils/deepUnwrap.ts
@@ -0,0 +1,26 @@
+import { toRaw } from 'vue';
+
+export function deepUnwrap(obj: any): any {
+  // Do not unwrap FileSystemFileHandle or FileSystemDirectoryHandle
+  if (
+    obj &&
+    (obj.constructor?.name === 'FileSystemFileHandle' ||
+     obj.constructor?.name === 'FileSystemDirectoryHandle')
+  ) {
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    return obj.map(deepUnwrap);
+  }
+  if (obj && typeof obj === 'object') {
+    if (typeof obj === 'function') return undefined;
+    if (obj.__v_isRef) return deepUnwrap(obj.value);
+    if (obj.__v_isReactive) obj = toRaw(obj);
+    const out: any = {};
+    for (const key in obj) {
+      out[key] = deepUnwrap(obj[key]);
+    }
+    return out;
+  }
+  return obj;
+} 
